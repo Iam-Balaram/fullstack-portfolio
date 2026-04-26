f4348d7b0d1ba932bee4d3932a36c2f8498b7c59
balarammukhi245-create <balarammukhi245@gmail.com>
2026-04-26 19:56:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Navbar } from "@/layout/Navbar";
+
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar/>
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
